db6dff26deb9f5b13712de25adc2d9c787930356
yevdem82 <evdem82@yandex.ru>
2023-08-17 03:12:52 +0300

    Add new IP to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,8 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.71.46', 'evdem82.zapto.org']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.71.46', '185.70.187.234']
 
 
 # Application definition
